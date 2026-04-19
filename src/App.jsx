a609399f9b1c0e11e14a8b48496d0ac6bc9b16cb
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-19 01:03:17 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Layout from "./components/Layout";
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId="6793546722-48rr78ea6tsderktj36dvihijcofjm8f.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="6793546722-ro4pd9ldsdrrl4l9k0jdm0pbl7f5ppu0.apps.googleusercontent.com">
       <BrowserRouter>
         <AuthProvider>
           <CarritoProvider>
