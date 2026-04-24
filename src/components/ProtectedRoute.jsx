6f4e04bc6e30de4c338623d4a86ecf6a90f03450
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-24 14:29:47 -0500

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,9 +2,10 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated, loading } = useAuth();
+  const { isAuthenticated, isReady } = useAuth();
 
-  if (loading) return null; 
+  // 🚫 Espera a que auth esté listo
+  if (!isReady) return null;
 
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 }
